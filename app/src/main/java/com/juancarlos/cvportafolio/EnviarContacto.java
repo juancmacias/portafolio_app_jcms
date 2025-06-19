3e89d2a4b5cae7c0c062017731135b410c9f95c2
juan carlos macias <juancmaciassalvador@gmail.com>
2025-06-19 09:23:48 +0200

    Varios cambios

--- app/src/main/java/com/juancarlos/cvportafolio/EnviarContacto.java
+++ app/src/main/java/com/juancarlos/cvportafolio/EnviarContacto.java
@@ -9,7 +9,15 @@ import androidx.appcompat.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.juancarlos.cvportafolio.app.AppConfig;
 import com.juancarlos.cvportafolio.app.BaseActivity;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class EnviarContacto extends BaseActivity {
 
@@ -29,6 +37,7 @@ public class EnviarContacto extends BaseActivity {
         buttonEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String key = "iM)J;NmDwn3[QTF~54?XA^EyuVsAiz.RayO&o7^g@z:.GNcnfF6_FM7V&KHi[";
                 String nombre = editTextNombre.getText().toString().trim();
                 String email = editTextEmail.getText().toString().trim();
                 String mensaje = editTextMensaje.getText().toString().trim();
@@ -36,11 +45,33 @@ public class EnviarContacto extends BaseActivity {
                 if (nombre.isEmpty() || email.isEmpty() || mensaje.isEmpty()) {
                     Toast.makeText(EnviarContacto.this, R.string.noenviado, Toast.LENGTH_SHORT).show();
                 } else {
-                    // Aquí podrías enviar el mensaje por email o a un servidor
-                    editTextNombre.setText("");
-                    editTextEmail.setText("");
-                    editTextMensaje.setText("");
-                    Toast.makeText(EnviarContacto.this, R.string.enviado, Toast.LENGTH_SHORT).show();
+                    StringRequest stringRequest = new StringRequest(Request.Method.POST, AppConfig.SEND,
+                            response -> {
+                                Toast.makeText(getApplicationContext(), R.string.enviado, Toast.LENGTH_LONG).show();
+                                // Aquí podrías enviar el mensaje por email o a un servidor
+                                editTextNombre.setText("");
+                                editTextEmail.setText("");
+                                editTextMensaje.setText("");
+                            },
+                            error -> {
+                                Toast.makeText(getApplicationContext(), R.string.notenviado, Toast.LENGTH_LONG).show();
+                            }) {
+                        @Override
+                        protected Map<String, String> getParams() {
+                            Map<String, String> params = new HashMap<>();
+                            params.put("api_key", key); // La misma que en el PHP
+                            params.put("nombre", nombre);
+                            params.put("email", email);
+                            params.put("mensaje", mensaje);
+                            return params;
+                        }
+                    };
+
+                    // Agrega la solicitud a la cola
+                    RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+                    requestQueue.add(stringRequest);
+
+
                 }
             }
         });
--- app/src/main/java/com/juancarlos/cvportafolio/app/AppConfig.java
+++ app/src/main/java/com/juancarlos/cvportafolio/app/AppConfig.java
@@ -4,5 +4,6 @@ public class AppConfig {
     public static String INICIO = "principal.html";
     public static String POLICITA = "politica.html";
     public static String URL = "https://www.juancarlosmacias.es/";
-    public static String JSONURL = "https://www.juancarlosmacias.es/api/porfolio/datos_proyectos.json";
+    public static String JSONURL = URL + "api/porfolio/datos_proyectos.json";
+    public static String SEND = URL + "api/send.php";
 }
